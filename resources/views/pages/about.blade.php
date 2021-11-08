4179318454e828033d7da09c36b613cee636c273
unknown <filipou43@gmail.com>
2021-11-08 19:39:51 +0200

    Added my project

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+        <h1>about</h1>
+        <p>this is a blog<p>
+@endsection
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+        <h1>welcome to blog</h1>
+        <p>this is a blog<p>
+@endsection 
